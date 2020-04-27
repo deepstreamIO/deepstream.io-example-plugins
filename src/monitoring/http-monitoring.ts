3b31f228c3b6663e7914654ad18aba2f4965a09c
jaime-ez <jechegarayz@gmail.com>
2020-04-27 14:04:36 -0400

    [task] document socketUserData for onMessageReceived callback param

--- src/monitoring/http-monitoring.ts
+++ src/monitoring/http-monitoring.ts
@@ -1,5 +1,5 @@
 import { Message, ACTIONS } from '@deepstream/protobuf/dist/types/messages'
-import { TOPIC, STATE_REGISTRY_TOPIC } from '@deepstream/protobuf/dist/types/all'
+import { TOPIC, STATE_REGISTRY_TOPIC, JSONObject } from '@deepstream/protobuf/dist/types/all'
 import { DeepstreamPlugin, DeepstreamMonitoring, DeepstreamServices, LOG_LEVEL, EVENT, DeepstreamHTTPMeta, DeepstreamHTTPResponse } from '@deepstream/types'
 
 interface HTTPMonitoringOptions {
@@ -54,7 +54,11 @@ export default class HTTPMonitoring extends DeepstreamPlugin implements Deepstre
         allowed ? stats.allowed++ : stats.declined++
     }
 
-    public onMessageReceived (message: Message): void {
+/**
+ * Called when an authorized message is received
+ * socketUserData passes the available info from socketWrapper { userId, serverData, clientData }
+ */
+    public onMessageReceived (message: Message, socketUserData: JSONObject): void {
         let actionsMap = this.receiveStats[TOPIC[message.topic]]
         if (!actionsMap) {
             actionsMap = {}
